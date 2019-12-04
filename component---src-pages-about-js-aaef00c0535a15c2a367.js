127ce86fe3129b4aee4be51969577233907c9c43
theingredients <theingredientscollective@gmail.com>
2019-12-04 14:43:44 -0800

    Updates

---
+++ component---src-pages-about-js-aaef00c0535a15c2a367.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"3XHS":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),r=n.n(a),c=n("LbRr"),o=n("Wbzz");t.default=function(){return r.a.createElement("div",{style:{color:"teal"}},r.a.createElement(o.Link,{to:"/"},"Home"),r.a.createElement(c.a,{headerText:"About Gatsby"}),r.a.createElement("p",null,"Such wow. Very React."))}},LbRr:function(e,t,n){"use strict";var a=n("q1tI"),r=n.n(a);t.a=function(e){return r.a.createElement("h1",null,e.headerText)}}}]);
+//# sourceMappingURL=component---src-pages-about-js-aaef00c0535a15c2a367.js.map
